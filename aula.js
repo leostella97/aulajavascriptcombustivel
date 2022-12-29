1551ed85a849bfbbfd9d14627f0262be9c6b3bf2
Leonardo Stella de Oliveira <Leonstella97@gmail.com>
2022-12-29 12:37:31 -0300

    Primeiro commit inserindo o arquivo .js

---
+++ aula.js
@@ -0,0 +1,26 @@
+
+let variavel = 10;
+const nome = "Leonardo Stella";
+
+console.log("Imprimir algo");
+console.log(variavel);
+
+variavel = 20;
+console.log(variavel);
+console.log(nome);
+
+let x = 2;
+let y = 3;
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x^y);
+
+//desafio viagem e combustível
+let precoCombustivel = 4.93;
+let gastoCarro = 10;
+let kmViagem = 100;
+let gastoViagem = precoCombustivel*(kmViagem/gastoCarro);
+
+console.log("O gasto da viagem foi: ", gastoViagem);
